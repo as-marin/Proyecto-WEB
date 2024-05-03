8f1d3418619a85afab77da05fe5fe946b58e7024
as-marin <agus.sepulveda@duocuc.cl>
2024-05-02 20:48:43 -0400

    Carrito validar cantidades
    carrito ya no deja escribir . o -.

--- js/cantProductos.js
+++ js/cantProductos.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+function limpiarSiMenorOIgualCero() {
     var cantidadInput = document.getElementById('idcantidad');
+    cantidadInput.value = cantidadInput.value.replace(/[^0-9]/g, ''); // Eliminar caracteres no deseados
+    var cantidad = cantidadInput.value.trim(); // Eliminar espacios en blanco al inicio y al final
+    var regex = /^[1-9]$/; // Expresión regular para aceptar solo un dígito entre 1 y 9
 
-    cantidadInput.addEventListener('change', function() {
-        var cantidad = parseInt(cantidadInput.value);
-
-        if (cantidad <= 0 || cantidad > 9) {
-            cantidadInput.value = ''; // Limpiar el campo si es menor o igual a 0
-        }
-    });
-});
+    if (!regex.test(cantidad)) {
+        cantidadInput.value = ''; // Limpiar el campo si no cumple con la expresión regular
+    }
+}
